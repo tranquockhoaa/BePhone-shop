e695255fb98322fb59eb4e220c5d5defd3f7c7f5
tranquockhoa <khoatran1.tt.nd@gmail.com>
2025-08-18 08:40:50 +0700

    feat: update

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const authController = require('../controllers/authController');
+const express = require("express");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 router.patch(
-  '/updatePassword',
+  "/updatePassword",
   authController.protect,
   authController.updatePassword,
-  authController.logout,
+  authController.logout
 );
 // router.get(
 //   '/:fullName',
 //   authController.protect,
 //   userController.getUserByFullName,
 // );
-router.post('/logout/:type', authController.logout);
-router.post('/logout', authController.logout);
+router.post("/logout/:type", authController.logout);
+router.post("/logout", authController.logout);
 // routes/authRouter.js
-router.post('/register-admin', authController.registerAdmin);
+router.post("/register-admin", authController.registerAdmin);
 module.exports = router;
